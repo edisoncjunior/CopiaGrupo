736d796dca895e3235babff38603d1e576e473f9
Edison Carvalho <edisoncjunior@gmail.com>
2026-02-03 14:53:22 -0300

    CopiaGrupoLog0h

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-﻿# VERSAO 2 Main CopiaGrupo BINANCE 
-# Com LOG e envio diário as 00h funcionando
-# T
-# Bruno Aguiar - MEXC com Taxa Zero
+﻿# VERSAO 1 Main CopiaGrupo - FUNCIONA LOCAL e WEB - 
+# funcionando Local e Railway
+# Com LOG e envio diário as 00h 
+# Com teste lógico de moedas (False/True) [Tudo ou Bruno Aguiar - MEXC com Taxa Zero]
+# redução de moedas para teste rodar em railway
+# foi testado rodar na Binance - bloqueio pela exchange!
 
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
@@ -13,14 +15,10 @@ from datetime import datetime, timezone, timedelta
 from telethon import TelegramClient, events
 from telethon.sessions import StringSession
 
-from binance.client import Client
-from binance.enums import *
-from binance.exceptions import BinanceAPIException
-
-
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 LOG_DIR = os.path.join(BASE_DIR, "logs")
 
+#SESSION_STRING = os.getenv("TELEGRAM_SESSION_STRING")
 TZ_BRASILIA = timezone(timedelta(hours=-3))
 last_sent_date = None
 
@@ -29,9 +27,8 @@ last_sent_date = None
 # -------------------------------------------------
 ALLOWED_SYMBOLS = {
 #Lista dos ativos do Bruno Aguiar na MEXC com taxa zero:
-#    "BCHUSDT", "BNBUSDT", "CHZUSDT", "DOGEUSDT", "ENAUSDT", "ETHUSDT",
-#    "JASMYUSDT", "SOLUSDT", "UNIUSDT", "XMRUSDT", 
-    "XRPUSDT"
+    "BCHUSDT", "BNBUSDT", "CHZUSDT", "DOGEUSDT", "ENAUSDT", "ETHUSDT",
+    "JASMYUSDT", "SOLUSDT", "UNIUSDT", "XMRUSDT", "XRPUSDT"
 }
  
 # -------------------------------------------------
@@ -58,9 +55,6 @@ SOURCE_CHAT_ID = int(os.environ["SOURCE_CHAT_ID"])
 TARGET_CHAT_ID = int(os.environ["TARGET_CHAT_ID"])
 SESSION_STRING = os.environ["TELEGRAM_SESSION_STRING"]
 
-BINANCE_API_KEY = os.environ["BINANCE_API_KEY"]
-BINANCE_API_SECRET = os.environ["BINANCE_API_SECRET"]
-
 # -------------------------------------------------
 # Cliente Telegram
 # -------------------------------------------------
@@ -69,16 +63,6 @@ client = TelegramClient(
     API_ID,
     API_HASH
 )
-
-# -------------------------------------------------
-# Cliente Binance
-# -------------------------------------------------
-def get_binance_client():
-    return Client(
-        BINANCE_API_KEY,
-        BINANCE_API_SECRET
-    )
-
 # ---------------------------------------------------
 # Extrai dados estruturados da mensagem do Telegram.
 # Retorna dict ou None se não casar com o padrão.
@@ -113,176 +97,6 @@ def parse_signal_message(text: str):
         "timeframe": timeframe,
         "price": price
     }
-# -------------------------------------------------------------------------
-# Função que verifica se já possui uma posição na Binance
-# -------------------------------------------------------------------------
-def has_open_position(symbol: str, position_side: str) -> bool:
-    """
-    Retorna True se já existir posição aberta (qty != 0)
-    """
-    binance = get_binance_client()
-
-    try:
-        positions = binance.futures_position_information(symbol=symbol)
-
-        for pos in positions:
-            if (
-                pos["symbol"] == symbol
-                and pos["positionSide"] == position_side
-                and float(pos["positionAmt"]) != 0.0
-            ):
-                return True
-
-        return False
-
-    except BinanceAPIException as e:
-        print(f"[BINANCE][POSITION][ERRO] {e}")
-        return True  # segurança: assume que existe
-
-# -------------------------------------------------------------------------
-# Função cria ordem Binance
-# -------------------------------------------------------------------------
-def create_binance_order(signal: dict):
-    binance = get_binance_client()
-    symbol = signal["symbol"]
-    signal_type = signal["signal"]
-
-    # ---- MAPEAMENTO DO SINAL ----
-    if "compra" in signal_type:
-        side = SIDE_BUY
-        position_side = "LONG"
-    elif "venda" in signal_type:
-        side = SIDE_SELL
-        position_side = "SHORT"
-    else:
-        print("[BINANCE] Sinal não reconhecido")
-        return
-    # ---------------------------------
-    # BLOQUEIO: posição já aberta
-    # ---------------------------------
-    if has_open_position(symbol, position_side):
-        print(
-            "[BINANCE][SKIP] Posição já aberta | "
-            f"Symbol={symbol} | Position={position_side}"
-        )
-        return
-
-
-    # ---- CONFIGURAÇÕES FIXAS (AJUSTE SE QUISER) ----
-    quantity = 10  # contratos
-    leverage = 10  # alavancagem
-
-    try:
-        # Setar alavancagem
-        binance.futures_change_leverage(
-            symbol=symbol,
-            leverage=leverage
-        )
-
-        order = binance.futures_create_order(
-            symbol=symbol,
-            side=side,
-            positionSide=position_side,
-            type=ORDER_TYPE_MARKET,
-            quantity=quantity
-        )
-
-        # ------------------------------
-        # CONFIRMA EXECUÇÃO
-        # ------------------------------
-        if not order_filled(order):
-            print("[BINANCE] Ordem criada mas não executada ainda")
-            return
-
-        entry_price = float(order.get("avgPrice"))
-        executed_qty = float(order.get("executedQty"))
-
-        print(
-            "[BINANCE] ORDEM CONFIRMADA | "
-            f"Symbol={symbol} | "
-            f"Entry={entry_price} | "
-            f"Qty={executed_qty}"
-        )
-
-        # ------------------------------
-        # CRIA TAKE PROFIT 50%
-        # ------------------------------
-        create_take_profit_50(
-            symbol=symbol,
-            position_side=position_side,
-            entry_price=entry_price,
-            qty=executed_qty,
-            leverage=leverage
-        )
-
-
-        order_price = order.get("avgPrice") or order.get("price") or "market"
-        executed_qty = order.get("executedQty")
-        order_id = order.get("orderId")
-        client_order_id = order.get("clientOrderId")
-        status = order.get("status")
-
-        print(
-            "[BINANCE] ORDEM EXECUTADA | "
-            f"Symbol={symbol} | "
-            f"Side={side} | "
-            f"Position={position_side} | "
-            f"Qty={executed_qty} | "
-            f"Preço={order_price} | "
-            f"Status={status} | "
-            f"OrderID={order_id}"
-        )
-
-        return order
-
-    except BinanceAPIException as e:
-        print(f"[BINANCE][ERRO] {e}")
-
-# -------------------------------------------------------------------------
-# Função que confirma se ordem foi criada na Binance
-# -------------------------------------------------------------------------
-def order_filled(order: dict) -> bool:
-    binance = get_binance_client()
-    return order.get("status") in ("FILLED", "PARTIALLY_FILLED")
-
-# -------------------------------------------------------------------------
-# Função que cria TP 50%
-# -------------------------------------------------------------------------
-def create_take_profit_50(symbol, position_side, entry_price, qty, leverage):
-    binance = get_binance_client()
-    TP_PERCENT = 0.50  / leverage # 50%
-
-    if position_side == "LONG":
-        tp_price = round(entry_price * (1 + TP_PERCENT), 6)
-        side = SIDE_SELL
-    else:  # SHORT
-        tp_price = round(entry_price * (1 - TP_PERCENT), 6)
-        side = SIDE_BUY
-
-    try:
-        tp_order = binance.futures_create_order(
-            symbol=symbol,
-            side=side,
-            positionSide=position_side,
-            type=ORDER_TYPE_TAKE_PROFIT_MARKET,
-            stopPrice=tp_price,
-            closePosition=False,
-            quantity=qty,
-            workingType="MARK_PRICE"
-        )
-
-        print(
-            "[BINANCE][TP] Criado | "
-            f"Symbol={symbol} | "
-            f"Position={position_side} | "
-            f"Qty={qty} | "
-            f"TP_Price={tp_price}"
-        )
-
-        return tp_order
-
-    except BinanceAPIException as e:
-        print(f"[BINANCE][TP][ERRO] {e}")
 
 # -------------------------------------------------------------------------
 # Função que retorna a data
@@ -341,10 +155,6 @@ async def forward_message(event):
                 print(f"[SKIP] Moeda ignorada: {parsed['symbol']}")
                 return  # não envia para o Telegram
 
-            # -------- ENTRADA BINANCE --------
-            if parsed["exchange"] == "BINANCE":
-                print("[BINANCE] Criando ordem...")
-                create_binance_order(parsed)
         else:
             # mensagem não é sinal → ignora encaminhamento
             return
